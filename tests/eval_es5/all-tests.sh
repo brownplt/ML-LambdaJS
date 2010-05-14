6ae78f3b2e22597250586929699779ad4c5497f9
joseph <joe@cs.brown.edu>
2010-05-14 14:24:43 -0400

    [es5] Change to shell scripts for running tests.

--- tests/eval_es5/all-tests.sh
+++ tests/eval_es5/all-tests.sh
@@ -8,7 +8,7 @@ echo ""
 
 for FILE in $P/*.js
 do
-    $P/../../src/jsc $FILE -env $P/test_env -env $P/../../data/es5-lib.es5 -full-desugar -eval
+    $P/single-test.sh $FILE
 done
 
 echo ""
@@ -17,6 +17,6 @@ echo "es5 tests:"
 
 for FILE in $P/*.es5
 do
-    $P/../../src/jsc $FILE -env $P/test_env -env $P/../../data/es5-lib.es5 -full-desugar -eval
+    $P/single-test.sh $FILE
 done
 
---
+++ tests/eval_es5/single-test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+P=`dirname $0`
+FILE=$1
+
+$P/../../src/jsc $FILE -env $P/test_env -env $P/../../data/es5-lib.es5 -full-desugar -eval
